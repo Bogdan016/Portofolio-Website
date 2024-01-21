bbbec0f1c7f6fee7b51df46603bf7f1175b489d3
Bogdan <bogdanistrate03@gmail.com>
2024-01-21 23:38:43 +0200

    implemented qualitication section and other improvements

--- assets/js/index.js
+++ assets/js/index.js
@@ -50,9 +50,26 @@ function toggleSkils(){
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkils)
 })
+
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
 
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 /*==================== SERVICES MODAL ====================*/
 
 
