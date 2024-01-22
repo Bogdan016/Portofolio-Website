301c64acca1dc67e9d92206b0a123ba5e4a90c7e
Bogdan <bogdanistrate03@gmail.com>
2024-01-23 00:21:30 +0200

    project section and other improvements

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,6 +74,19 @@ tabs.forEach(tab =>{
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portofolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 
 /*==================== TESTIMONIAL ====================*/
